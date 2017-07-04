13ec1a4bc89adb8c9741b3a21ad2fa37d52f3c93
Nebojsa Martinovic <martinovn1975@gmail.com>
2017-07-04 16:57:55 +0200

    Config file

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php 
+//kreiramo konstante za parametre koje ubacujemo za konekciju sa bazom pri instanciranju novog PDO objekta
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "");
+define("DB_NAME", "calmy");
+
+//definisemo konstante za custom url
+//define url
+define("ROOT_PATH", "/calm_project/");
+define("ROOT_URL", "http://localhost/calm_project/");
+
+
+ ?>
